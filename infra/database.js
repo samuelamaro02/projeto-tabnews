13d899a930aa5636f6170738c3bacfd615ab9f75
samuelamaro02 <134892999+samuelamaro02@users.noreply.github.com>
2025-03-05 17:29:21 +0000

    adiciona codigo SSL ideal para NeonSQL no

--- infra/database.js
+++ infra/database.js
@@ -1,44 +1,39 @@
-import { Client } from 'pg'; 
+import { Client } from 'pg';
 
-async function query(queryObject){
+async function query(queryObject) {
     const client = new Client({
         host: process.env.POSTGRES_HOST,
         port: process.env.POSTGRES_PORT,
         user: process.env.POSTGRES_USER,
         database: process.env.POSTGRES_DB,
         password: process.env.POSTGRES_PASSWORD,
-        ssl: process.env.NODE_ENV === 'development' ? false : true,
-
+        ssl: {
+            rejectUnauthorized: false, // Permite conexão com certificados autoassinados
+        },
     });
+    
     console.log('Credenciais do Postgres:', {
         host: process.env.POSTGRES_HOST,
         port: process.env.POSTGRES_PORT,
         user: process.env.POSTGRES_USER,
         database: process.env.POSTGRES_DB,
         password: process.env.POSTGRES_PASSWORD,
-    })
-    
+        ssl: { rejectUnauthorized: false }, // Exibe a configuração SSL no log
+    });
+   
 
     try {
-        await client.connect(); 
-        const  result = await client.query(queryObject);
-        return result;   
-
-    } catch(error){
+        await client.connect();
+        const result = await client.query(queryObject);
+        return result;
+    } catch (error) {
         console.error(error);
         throw error;
-
-    }
-    finally {
+    } finally {
         await client.end();
     }
-
 }
 
-
-
 export default {
-    query:query,
-}; 
-   
- 
+    query: query,
+};
--- tests/integration/api/v1/status/get.test.js
+++ tests/integration/api/v1/status/get.test.js
@@ -9,7 +9,7 @@ test("GET to /api/v1/status should return to 200", async () => {
   expect(responseBody.updated_at).toEqual(parsedUpdateAt)
 
   expect(responseBody.dependencies.database.version).toEqual("16.8")
-  expect(responseBody.dependencies.database.max_connections).toEqual(100)
+  expect(responseBody.dependencies.database.max_connections).toEqual(901)
   expect(responseBody.dependencies.database.opened_connections).toEqual(1)
 
 });
